98f89b8903455382898d3ef524ff35621c39a892
virabbia <64023028+virabbia@users.noreply.github.com>
2024-11-04 20:52:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Replace with your actual Client ID and Redirect URI
 const CLIENT_ID = '0e507d976bac454da727e5da965c22fb';
-const REDIRECT_URI = 'http://localhost:5500/callback.html';
+const REDIRECT_URI = 'https://virabbia.github.io/mystery-song-player/callback.html';
 let token;
 
 // Function to get the track URI from the URL query parameter or `state` after redirect
