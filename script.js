752a744a82f5ff9cad5d9aadd489350ee17a47e4
virabbia <64023028+virabbia@users.noreply.github.com>
2024-11-05 19:36:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,28 +2,8 @@
 const CLIENT_ID = '0e507d976bac454da727e5da965c22fb';
 const REDIRECT_URI = 'https://virabbia.github.io/mystery-song-player/callback.html';
 
-// Authentication function to get Spotify access token
-function authenticate() {
-    const authUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=streaming%20user-read-playback-state%20user-modify-playback-state`;
-    window.location.href = authUrl;
-}
-
-// Extract token from URL after authentication
-function getTokenFromUrl() {
-    const hash = window.location.hash;
-    if (hash) {
-        const params = new URLSearchParams(hash.substring(1));
-        return params.get('access_token');
-    }
-    return null;
-}
-
-// Main: Check if we have a token or need to authenticate
-const token = getTokenFromUrl();
-if (!token) {
-    console.log("No token found, redirecting to authenticate...");
-    authenticate();
-} else {
-    console.log("Token found:", token);
-    // You can use this token to call Spotify's API
+// Function to get track URI from the URL
+function getTrackUri() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('track');
 }
